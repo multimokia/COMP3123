26ae18b737c98e1f6b3f41b9717ba07161640a1f
multimokia <multimokia3@gmail.com>
2022-09-16 16:47:43 -0400

    fixes

--- lab1/src/index.js
+++ lab1/src/index.js
@@ -1,64 +1,64 @@
-//Exercise 1
+// Exercise 1
 /**
  *
  * @param {string} phrase The phrase to titlize
  */
-function titleize(phrase) {
-    let words = phrase.split(' ');
+function titleize (phrase) {
+  const words = phrase.split(' ')
 
-    //Update all the words to be capitalized
-    for (const [ind, word] of words.entries()) {
-        words[ind] = `${word.charAt(0).toUpperCase()}${word.substring(1)}`;
-    }
+  // Update all the words to be capitalized
+  for (const [ind, word] of words.entries()) {
+    words[ind] = `${word.charAt(0).toUpperCase()}${word.substring(1)}`
+  }
 
-    return words.join(" ");
+  return words.join(' ')
 }
 
 /**
  * @param {Number} arguments The numbers to find the maximum of
  */
-function getLargest() {
-    //Min must be -inf
-    let maximum = -Infinity;
+function getLargest () {
+  // Min must be -inf
+  let maximum = -Infinity
 
-    for (const num of arguments) {
-        if (num > maximum) {
-            maximum = num;
-        }
+  for (const num of arguments) {
+    if (num > maximum) {
+      maximum = num
     }
+  }
 
-    return maximum;
+  return maximum
 }
 
 /**
  *
  * @param {string} word Word to move the last 3 letters of
  */
-function moveLastThreeLetters(word) {
-    //Word length must be >= 3
-    if (word.length < 3) {
-        return word;
-    }
+function moveLastThreeLetters (word) {
+  // Word length must be >= 3
+  if (word.length < 3) {
+    return word
+  }
 
-    return `${word.substring(word.length - 3)}${word.substring(0, word.length - 3)}`;
+  return `${word.substring(word.length - 3)}${word.substring(0, word.length - 3)}`
 }
 
 /**
  *
  * @param {Number} angleDegrees degree to classify
  */
-function angleClassifier(angleDegrees) {
-    if (angleDegrees < 90) { return "Acute angle"; }
-    if (angleDegrees > 90 && angleDegrees < 180) { return "Obtuse angle"; }
-    if (angleDegrees == 180) { return "Straight angle" }
+function angleClassifier (angleDegrees) {
+  if (angleDegrees < 90) { return 'Acute angle' }
+  if (angleDegrees > 90 && angleDegrees < 180) { return 'Obtuse angle' }
+  if (angleDegrees === 180) { return 'Straight angle' }
 
-    //Fallback option
-    return "Right angle"
+  // Fallback option
+  return 'Right angle'
 }
 
 module.exports = {
-    titleize: titleize,
-    getLargest: getLargest,
-    moveLastThreeLetters: moveLastThreeLetters,
-    angleClassifier: angleClassifier
+  titleize,
+  getLargest,
+  moveLastThreeLetters,
+  angleClassifier
 }
